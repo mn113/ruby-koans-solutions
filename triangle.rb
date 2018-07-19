12a370e7697308684b018d840ef2eab82978bacd
Martin Nicholson <recyclebing+github@gmail.com>
2018-07-19 12:49:09 +0100

    Wrote triangle error checks

--- triangle.rb
+++ triangle.rb
@@ -1,5 +1,9 @@
 # Triangle Project Code.
 
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
+
 # Triangle analyzes the lengths of the sides of a triangle
 # (represented by a, b and c) and returns the type of triangle.
 #
@@ -14,7 +18,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && a == c
+  side_sum = [a,b,c].reduce(:+)
+  long_side = [a,b,c].max
+
+  # Check for invalid inputs first:
+  if [a,b,c].any? { |x| x <= 0 }
+    raise TriangleError #"Negative or zero-length side"
+  elsif side_sum <= long_side * 2
+    raise TriangleError #"Invalid sides"
+
+  # Then categorise:
+  elsif a == b && a == c
     # all 3 sides are equal
     return :equilateral
   elsif (a != b) && (a != c) && (b != c)
@@ -24,8 +38,4 @@ def triangle(a, b, c)
     # by elimination, 2 sides must be equal
     return :isosceles
   end
-end
-
-# Error class used in part 2.  No need to change this code.
-class TriangleError < StandardError
 end
